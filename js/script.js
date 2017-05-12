c59647ab912226fd651b5e1a3ea23837355c8451
Skyler Rexroad <skyrex1095@gmail.com>
2017-05-11 20:36:35 -0600

    Finished!

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 var container = $('#container')
 var details = [
 	{color: '#fff5ba', text: "Hello, mom!"},
-	{color: '#ffffd1', text: "2"},
-	{color: '#dbffd6', text: "3"},
-	{color: '#bffcc6', text: "4"},
-	{color: '#aff8db', text: "5"},
-	{color: '#c4faf8', text: "6"},
-	{color: '#85e3ff', text: "You're invited to something blah blah text filler here!"}
+	{color: '#ffffd1', text: "I’m glad that you’re my mother,"},
+	{color: '#dbffd6', text: "kind and caring and strong."},
+	{color: '#bffcc6', text: "'Cause surely no one else,"},
+	{color: '#aff8db', text: "could have put up with me this long!"},
+	{
+		color: '#85e3ff',
+		text: "You're invited to a relaxing day out!"
+			+ "<span>AGENDA:</span>"
+			+ "<span>10:00 am &mdash; Go get some coffee or tea</span>"
+			+ "<span>11:00 am &mdash; Walk around the Railyards Market</span>"
+			+ "<span>1:00 pm &mdash; Have lunch at St. Clair's Winery and Bistro</span>"
+			+ "<span>3:00ish pm &mdash; Your choice of live music! (We've found several events around town)</span>"
+			+ "<br /><span>Love you, mom!</span>"
+			+ "<span>We'll pick you up at 10:00 am on Sunday.</span>"
+			+ "<br /><button class=\"start-over\" onclick=\"goToStart()\">Start Over</button>"
+	}
 ]
 var layers = []
 
@@ -14,10 +24,10 @@ elsToAppend = []
 details.forEach(function(detail, i) {
 	var el = $("<div></div>", {
 		"id": 'layer' + i,
-		"style": 'background: ' + detail.color
+		"style": 'background-color: ' + detail.color + ';' + 'background-image: url("img/floral-bg-transparent.png");'
 	})
 	var notLast = i < details.length - 1
-	el.html(detail.text + (notLast ? "<span>(Click to continue...)</span>" : ""))
+	el.html(detail.text + (notLast ? "<span class='click'>(Click to continue...)</span>" : ""))
 	if (notLast) {
 		el.addClass('clickable')
 		el.click(function() {
@@ -27,3 +37,7 @@ details.forEach(function(detail, i) {
 	elsToAppend.push(el)
 })
 container.append(elsToAppend.reverse())
+
+function goToStart() {
+	$('div[id^="layer"]').removeClass('offscreen')
+}
